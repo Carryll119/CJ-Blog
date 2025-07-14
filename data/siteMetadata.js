1a0e8cc6af55c9344e29dd17d53c65370b898c28
Carryll119 <carryll119@gmail.com>
2025-07-14 20:08:02 +0800

    fix: update siteUrl to netlify domain

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: '梳理永續轉型中的變化，理解它如何影響我們的選擇與生活',
   language: 'zh-TW',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://carry-all-greenotes.netlify.app',
+  siteRepo: 'https://github.com/Carryll119/Carry-All-Greenotes',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
